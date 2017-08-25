b9f3c986d2356ddb98b11e618439acab44a0922b
codingpanda23 <league@WTS20.attlocal.net>
2017-08-25 12:08:41 -0700

    Level 0 Test

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
@@ -6,14 +7,42 @@ import org.jointheleague.graphical.robot.Robot;
 public class CodingExercise1 {
 
     public static void main(String[] args) {
+		Robot ada = new Robot();
+		int red = 0;
+		int yellow = 0;
+		int blue = 0;
+		int green = 0;
    	 // 3. ask the user what color they would like the Robot to draw
-
+    		String color = JOptionPane.showInputDialog("What color do you want the robot to draw with? Your options are red, yellow, blue, or green.");
+    		
    	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
-
+    		if (color.equals("red")) {
+			ada.setPenColor(255, 0, 0);	
+		}
+    		else if (color.equals("green")) {
+			ada.setPenColor(0, 255, 0);	
+		}
+    		else if (color.equals("blue")) {
+			ada.setPenColor(0, 0, 255);	
+		}
+    		else if (color.equals("yellow")) {
+			ada.setPenColor(255, 243, 0);	
+		}
    	 // 2. set the pen width to 10
-
+    		ada.setPenWidth(10);
    	 // 1. make the Robot draw a shape
-   	 
+   	 	ada.moveTo(300, 400);
+   	 	ada.setSpeed(100);
+   	 	ada.penDown();
+   	 	ada.move(200);
+   	 	ada.turn(90);
+   	 	ada.move(200);
+   	 	ada.turn(90);
+   	 	ada.move(200);
+	 	ada.turn(90);
+	 	ada.move(200);
+   	 	ada.turn(90);
+   	 	ada.penUp();
     }
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -10,8 +10,18 @@ import javax.swing.JOptionPane;
  **/
 
 public class CodingExercise2 {
-
+public static void main(String[] args) {
+	String age = JOptionPane.showInputDialog("How old are you?");
+	int age2 = Integer.parseInt(age);
+	if (age2 > 30) {
+		JOptionPane.showMessageDialog(null, "WHAT ARE YOU THINKING? YOU'RE TOO OLD FOR THIS!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "You were born in " + (2017-age2));
+	}
+	}
 }
+
 
 
 
